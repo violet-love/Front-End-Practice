c9d47ec1650b526d6bc5700323187314041d2108
Matthew Gammon <mgammon91@gmail.com>
2021-07-15 19:20:36 -0400

    imitial commit. comments display at the bottom of the posts list

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <div className="d-flex flex-column align-items-center">
+        <div>
+          <h1>Practice Application</h1>
+        </div>
+        <div>
+          <p>
+            This app is being created as a practice application for the Front
+            End interview.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
